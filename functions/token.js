89a29d1eefd7ce956a4c04a56b467268fef629a9
Luis Marsiglia <marsiglia.business@gmail.com>
2022-07-07 12:38:29 -0400

    first commit

---
+++ functions/token.js
@@ -0,0 +1,42 @@
+exports.handler = function (context, event, callback) {
+  const twilioAccountSid = context.ACCOUNT_SID
+	const twilioApiKey = context.API_KEY
+	const twilioApiSecret = context.API_SECRET
+	const twilioServiceSid = context.SERVICE_SID
+  const identity = event.identity
+  
+  const { AccessToken } = Twilio.jwt
+  const { VideoGrant, ChatGrant } = AccessToken
+
+	const token = new AccessToken(
+		twilioAccountSid,
+		twilioApiKey,
+		twilioApiSecret,
+		{ identity }
+  )
+  
+	const videoGrant = new VideoGrant()
+  
+  const chatGrant = new ChatGrant({
+		serviceSid: twilioServiceSid
+  })
+  
+  token.addGrant(videoGrant)
+  token.addGrant(chatGrant)
+
+  const response = new Twilio.Response()
+	const headers = {
+		// 'Access-Control-Allow-Origin': '*', // change this to your client-side URL
+		'Access-Control-Allow-Origin': 'http://localhost:8000,https://marsidev-wathsup.deno.dev',
+		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+		'Access-Control-Allow-Headers': 'Content-Type',
+		'Content-Type': 'application/json'
+	}
+
+	response.setHeaders(headers)
+	response.setBody({
+		accessToken: token.toJwt()
+	})
+
+	return callback(null, response)
+}
